7570c42c5b1c3977d1369ca8c5e1a2ee74a58d97
lucasljoliveira-olist <lucasjoao.oliveira@olist.com>
2021-02-09 13:51:38 -0300

    Created project and models for devolution api

---
+++ devolution_api/devolution/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from datetime import datetime
+
+# Create your models here.
+
+class DevolutionReason(models.Model):
+    title = models.CharField(max_length=100, null=False, blank=False)
+    description = models.CharField(max_length=200, null=False, blank=False)
+
+class DevolutionStatus(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    description = models.CharField(max_length=200, null=False, blank=False)
+
+class DevolutionType(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    description = models.CharField(max_length=200, null=False, blank=False)
+
+class Devolution(models.Model):
+    id_order = models.IntegerField()
+    id_devolution_type = models.ForeignKey(
+        DevolutionType,
+        on_delete=models.RESTRICT
+    )
+    id_devolution_reason = models.ForeignKey(
+        DevolutionReason,
+        on_delete=models.RESTRICT
+    )
+    id_devolution_status = models.ForeignKey(
+        DevolutionStatus,
+        on_delete=models.RESTRICT
+    )
+    date = models.DateField(default=datetime.today)
+    buyer_reason = models.CharField(max_length=100, null=False, blank=False)
+
+
+class DevolutionProductOrder(models.Model):
+    id_order_product = models.IntegerField()
+    id_devolution = models.ForeignKey(
+        Devolution,
+        on_delete=models.RESTRICT
+    )
+    id_devolution_reason = models.ForeignKey(
+        DevolutionReason,
+        on_delete=models.RESTRICT
+    )
+
